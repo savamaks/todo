98dfa1a0b47457c2895c7f2d4035eb00b1ed56bb
Максим <105449753+savamaxxx@users.noreply.github.com>
2023-01-28 21:17:43 +0300

    a3

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
-
-
-
 function todoList() {
-    const input = document.querySelector(".header__form-input");
-    const buttonCreate = document.querySelector(".header__form-button");
-    const list = document.querySelector(".box__list");
-    let links = document.querySelectorAll('.box__list')
+  const input = document.querySelector(".header__form-input");
+  const buttonCreate = document.querySelector(".header__form-button");
+  const list = document.querySelector(".box__list");
+  let count = 0;
 
-    function createLi() {
-        if (input.value !== "") {
-            let linkList = `<div class="box__list-link">${input.value}<span class="close-button">\u00D7</span></div>`
-            list.innerHTML+=linkList
-            input.value = ""
-        }
+  if (localStorage.length !== 0) {
 
-        console.log(links)
+    for (let i = 0; i < localStorage.length; i++) {
+        
+      let key = localStorage.key(i);
+
+      const li = document.createElement("li");
+      const closeButton = document.createElement("button");
+      const textClose = document.createTextNode("\u00D7");
+      const textLi = document.createTextNode(`${localStorage.getItem(key)}`);
+      closeButton.className = "close-button";
+      li.className = "box__list-link";
+
+      closeButton.append(textClose);
+      li.append(textLi);
+      li.append(closeButton);
+      list.append(li);
+
+      li.addEventListener("click", () => {
+        li.classList.toggle("checked");
+      });
+
+      closeButton.addEventListener("click", () => {
+        li.remove();
+        let liText = li.textContent;
+        let liTextClear = liText.replace(/\u00D7/gi, "");
+        localStorage.removeItem(liTextClear);
+      });
     }
+  }
 
+  function createLi() {
+    if (input.value !== "") {
+      const li = document.createElement("li");
+      const closeButton = document.createElement("button");
+      const textClose = document.createTextNode("\u00D7");
+      const textLi = document.createTextNode(input.value);
+      closeButton.className = "close-button";
+      li.className = "box__list-link";
 
+      closeButton.append(textClose);
+      li.append(textLi);
+      li.append(closeButton);
+      list.append(li);
+      input.value = "";
 
-    buttonCreate.addEventListener("click", createLi);
+      localStorage.setItem(textLi.textContent, textLi.textContent);
+
+      li.addEventListener("click", () => {
+        li.classList.toggle("checked");
+      });
+
+      closeButton.addEventListener("click", () => {
+        li.remove();
+        let liText = li.textContent;
+        let liTextClear = liText.replace(/\u00D7/gi, "");
+        localStorage.removeItem(liTextClear);
+      });
+    }
+  }
+  buttonCreate.addEventListener("click", createLi);
 }
 
-todoList();
+document.addEventListener("DOMContentLoaded", todoList);
+
+// function createLi() {
+//     if (input.value !== "") {
+//       let linkList = `<div class="box__list-link">${input.value}<span class="close-button">\u00D7</span></div>`;
+//       list.innerHTML += linkList;
+//       input.value = "";
+//     }
+//     let closeButtons = document.querySelectorAll(".close-button");
+
+//     closeButtons.forEach((button) => {
+//       button.addEventListener("click", ()=>{
+//         deletElement(button)
+//         strikeOutElement(document.querySelector(".box__list-link"))
+//       });
+//     });
+//   }
+
+//   function deletElement(button) {
+//     let parentEl = button.parentElement
+//     parentEl.style.display = 'none'
+//   }
+
+//   function strikeOutElement(button) {
+
+//     button.addEventListener('click',()=>{
+//         button.classList.toggle("checked")
+//     })
+
+//   }
+
+//   buttonCreate.addEventListener("click", createLi);
